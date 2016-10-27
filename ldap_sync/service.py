8847512ec4ad736242c43b4e9ef665e2a4556f59
Alex Sandro <geniofuturo@gmail.com>
2016-10-27 16:20:47 -0200

    Changing some details of the interface and implementing the service.

--- ldap_sync/service.py
+++ ldap_sync/service.py
@@ -4,38 +4,50 @@ import sys
 class Service(object):
     """Interface"""
 
-    def __init__(self, ldap):
-        self.ldap = ldap
+    class ObjectTypes(object):
+        GROUPS = "groups"
+        USERS = "users"
 
-    def search(self, base, filter, attributes):
+    def __init__(self, uri):
+        pyad.set_defaults(ldap_server=uri)
+
+    def search(self, base, filter, attributes, objectype=None):
         """Make a search"""
         raise NotImplemented
 
     def login(self, username, password):
-        """Make login"""
-        raise NotImplemented
-
-    def __call__(self, uri, username=None, password=None, *args, **kwargs):
+        """bind"""
         raise NotImplemented
 
 
 if sys.platform.startswith("win"):
-    import pyad
+    from pyad import *
 
 
     class PyadService(Service):
+        """Interface implementation"""
 
-        def search(self, base, filter, attributes):
-            """"""
+        def _search_users(self, base, filter, attributes):
+            """Search for users"""
+            groups = adgroup.ADGroup.from_dn(filter + "," + base)
+            users = []
+            for user in groups.get_members():
+                users.append((user.cn, {k: (getattr(user, k),) for k in attributes}))
+            return users
+
+        def _search_groups(self, base, filter, attributes):
+            return []
+
+        def search(self, base, filter, attributes, objectype=None):
+            """generic search"""
+            return getattr(self, '_search_' + objectype)(base, filter, attributes)
 
         def login(self, username, password):
-            self.ldap.set_defaults(username=username, password=password)
-
-        def __call__(self, uri, *args, **kwargs):
-            self.ldap.set_defaults(ldap_server=uri)
+            """make login"""
+            pyad.set_defaults(username=username, password=password)
 
 
-    service = PyadService(pyad)
+    Service = PyadService
 else:
 
     raise NotImplemented
