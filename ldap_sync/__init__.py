f220e5b10fe2333af7172578e835fb48749995bc
alex <geniofuturo@gmail.com>
2017-07-31 17:27:43 -0300

    Configuring app config.

--- ldap_sync/__init__.py
+++ ldap_sync/__init__.py
@@ -1,2 +1,4 @@
+default_app_config = 'ldap_sync.apps.LdapSyncConfig'
+
 __version_info__ = (1, 3, 1)
 __version__ = '.'.join([str(v) for v in __version_info__])
---
+++ ldap_sync/apps.py
@@ -0,0 +1,7 @@
+from django.utils.translation import ugettext_lazy as _
+from django.apps import AppConfig
+
+
+class LdapSyncConfig(AppConfig):
+    name = 'ldap_sync'
+    verbose_name = _("Ldap sync")
