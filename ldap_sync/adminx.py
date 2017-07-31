7e8776545206c6c2b9797f0db9f321bebc92f1b0
alex <geniofuturo@gmail.com>
2017-07-31 16:34:24 -0300

    Implemented administration view.

---
+++ ldap_sync/adminx.py
@@ -0,0 +1,27 @@
+import logging
+from django.utils.translation import ugettext_lazy as _
+from xadmin import site
+from .models import LdapSyncLog, LdapSyncLogMeta
+
+
+class LdapSearchInline(object):
+    model = LdapSyncLogMeta
+    exclude = ("level",)
+    readonly_fields = ['text', 'level_text_show']
+    style = "table"
+    extra = 0
+
+    def level_text_show(self, obj):
+        return logging.getLevelName(obj.level)
+
+    level_text_show.short_description = _("Level")
+    level_text_show.allow_tags = True
+    level_text_show.is_column = True
+
+
+class LdapSearchAdmin(object):
+    """"""
+    inlines = (LdapSearchInline,)
+
+
+site.register(LdapSyncLog, LdapSearchAdmin)
