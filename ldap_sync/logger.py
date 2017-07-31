e7ff5c12ccae7d2f30187b0ce138bc3870d6b51b
alex <geniofuturo@gmail.com>
2017-07-31 15:07:24 -0300

    Implemented logger for the database.

---
+++ ldap_sync/logger.py
@@ -0,0 +1,26 @@
+import logging
+
+from ldap_sync.models import LdapSyncLog, LdapSyncLogMeta
+
+
+class Logger(object):
+    def __init__(self):
+        self.slog = LdapSyncLog.objects.create()
+
+    def log(self, msg, level=logging.INFO):
+        return LdapSyncLogMeta.objects.create(
+            log=self.slog,
+            level=level,
+            text=msg)
+
+    def info(self, msg):
+        self.log(msg)
+
+    def error(self, msg):
+        self.log(msg, logging.ERROR)
+
+    def debug(self, msg):
+        self.log(msg, logging.DEBUG)
+
+    def warning(self, msg):
+        self.log(msg, logging.WARNING)
