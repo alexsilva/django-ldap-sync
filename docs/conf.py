84093b7775adfea99f5d165cb699d7ac2c0a226a
alex <geniofuturo@gmail.com>
2020-03-06 11:07:21 -0300

    Update strings.

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'django-ldap-sync'
-copyright = u'2013, Jason Bittel'
+project = 'django-ldap-sync'
+copyright = '2013, Jason Bittel'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -183,8 +183,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'django-ldap-sync.tex', u'django-ldap-sync Documentation',
-   u'Jason Bittel', 'manual'),
+  ('index', 'django-ldap-sync.tex', 'django-ldap-sync Documentation',
+   'Jason Bittel', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'django-ldap-sync', u'django-ldap-sync Documentation',
-     [u'Jason Bittel'], 1)
+    ('index', 'django-ldap-sync', 'django-ldap-sync Documentation',
+     ['Jason Bittel'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'django-ldap-sync', u'django-ldap-sync Documentation',
-   u'Jason Bittel', 'django-ldap-sync', 'One line description of project.',
+  ('index', 'django-ldap-sync', 'django-ldap-sync Documentation',
+   'Jason Bittel', 'django-ldap-sync', 'One line description of project.',
    'Miscellaneous'),
 ]
 
