d669cdc91885ab5400860fb917cc4b2fc3787c0f
alexsilva <geniofuturo@gmail.com>
2024-01-11 12:50:33 -0300

    Signal registration.

--- ldap_sync/apps.py
+++ ldap_sync/apps.py
@@ -1,8 +1,13 @@
 # coding=utf-8
-from django.utils.translation import gettext_lazy as _
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class LdapSyncConfig(AppConfig):
-    name = 'ldap_sync'
-    verbose_name = _("Ldap sync")
+	name = 'ldap_sync'
+	verbose_name = _("Ldap sync")
+
+	def ready(self):
+		from ldap_sync.signal_register import ready
+
+		ready(self)
